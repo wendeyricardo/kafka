0485468e65359e96583b990b98ab7aa7f757519c
wendey <wendeyricardo@gmail.com>
2024-11-03 12:01:33 -0300

    enviando registro para o tópico

---
+++ src/main/java/com/wendey/str_producer/resources/StringProducerResource.java
@@ -0,0 +1,26 @@
+package com.wendey.str_producer.resources;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wendey.str_producer.services.StringProducerService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value ="/producer")
+public class StringProducerResource {
+
+    private final StringProducerService producerService;
+    
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message){
+        producerService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
---
+++ src/main/java/com/wendey/str_producer/services/StringProducerService.java
@@ -0,0 +1,17 @@
+package com.wendey.str_producer.services;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+    
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        kafkaTemplate.send("str-topic", message);
+    }
+}
