c6c3dfe58ae1a828287b6e759f50f7887e2269be
wendey <wendeyricardo@gmail.com>
2024-11-09 15:34:34 -0300

    add producer config

--- payment-service/src/main/java/com/wendey/payment_service/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/wendey/payment_service/services/impl/PaymentServiceImpl.java
@@ -1,19 +1,35 @@
 package com.wendey.payment_service.services.impl;
 
+import java.io.Serializable;
+
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.wendey.payment_service.model.Payment;
 import com.wendey.payment_service.services.PaymentService;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVISE_IMPL ::: Recebi o pagamento {}", payment);
+        log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("enviando pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
+
+
         
     }
     
